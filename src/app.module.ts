caf03e009e4b73653a828a826ffecb6a8df2755f
Thxn <155278099+thanchayawikgithub@users.noreply.github.com>
2025-01-14 10:27:48 +0700

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password',
+      database: 'oh-my-task',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
